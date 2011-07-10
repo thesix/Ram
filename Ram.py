1a7ff1ff1bcc00e0e96d8214ee7af28049ee9175
Jogi Hofmüller <j.hofmueller@student.tugraz.at>
2011-07-10 18:00:30 +0200

    went back to original operand coding
    As we were.  #k ist for constants, k for register numbers and *k for
    indirect addressing.

--- Ram.py
+++ Ram.py
@@ -34,16 +34,16 @@ ADD k   :=  ADD v(k) to accumulator
 SUB k   :=  SUBtract v(k) from accumulator
 MULT k  :=  MULTiply accumulator by v(k)
 DIV k   :=  DIVide accumulator by v(k)
-GOTO k  :=  GOTO line number v(k)
-JZERO k :=  Jump to line number v(k) if accumulator == ZERO (0)
+GOTO k  :=  GOTO line number k
+JZERO k :=  Jump to line number k if accumulator == ZERO (0)
 END ?   :=  set program counter to 0 (termination)
 
 Arguments:
 
 x     v(x)
 ------------
- k    k
-#k    reg[k]
+ k    reg[k]
+#k    k
 *k    reg[reg[k]]
 
 An example program can be found at the bottom of this file.  If you run
@@ -138,130 +138,130 @@ class Ram:
       space += s
     print "result(R)={0}, time(R)={1}, space(R)={2}".format (self.reg[0], self.time, space)
 
-  def load (self, arg):
+  def load (self, oper):
     """
     index errors are caught and reg will be expanded up to
-    the index in arg;  this satisfies the need of infinite registers
+    the index in oper;  this satisfies the need of infinite registers
     initialized with 0.
     """
     try:
-      self.reg[0] = self.argval (arg)
+      self.reg[0] = self.decoper (oper)
     except IndexError:
-      for i in range (len (self.reg), self.regindex (arg)):
+      for i in range (len (self.reg), self.regindex (oper)):
         self.reg.append (0)
         self.space.append (0)
       self.reg.append (0)
       self.space.append (0)
       self.reg[0] = 0
     self.pc += 1
-    self.addspace (arg)
-    self.addtime (arg)
+    self.addspace (oper)
+    self.addtime (oper)
 
-  def store (self, arg):
+  def store (self, oper):
     """
     index errors are caught and reg will be expanded up to
-    the index arg;  accumulator value will be stored at the
-    index indicated by arg.
+    the index oper;  accumulator value will be stored at the
+    index indicated by oper.
     """
     try:
-      self.reg[self.regindex (arg)] = self.reg[0]
+      self.reg[self.regindex (oper)] = self.reg[0]
     except IndexError:
-      for i in range (len (self.reg), self.regindex (arg)):
+      for i in range (len (self.reg), self.regindex (oper)):
         self.reg.append (0)
         self.space.append (0)
       self.reg.append (self.reg[0])
       self.space.append (self.cost (self.reg[0]))
     self.pc += 1
-    self.addtime (arg, True)
-    self.addspace (arg)
+    self.addtime (oper, True)
+    self.addspace (oper)
 
-  def add (self, arg):
-    self.reg[0] += self.argval (arg)
+  def add (self, oper):
+    self.reg[0] += self.decoper (oper)
     self.pc += 1
-    self.addtime (arg, True)
+    self.addtime (oper, True)
     self.addspace ("0")
 
-  def sub (self, arg):
-    if self.argval (arg) > self.reg[0]:
+  def sub (self, oper):
+    if self.decoper (oper) > self.reg[0]:
       self.reg[0] = 0
     else:
-      self.reg[0] -= self.argval (arg)
+      self.reg[0] -= self.decoper (oper)
     self.pc += 1
-    self.addtime (arg, True)
+    self.addtime (oper, True)
 
-  def mult (self, arg):
-    self.reg[0] *= self.argval (arg)
+  def mult (self, oper):
+    self.reg[0] *= self.decoper (oper)
     self.pc += 1
-    self.addtime (arg, True)
+    self.addtime (oper, True)
     self.addspace ("0")
 
-  def div (self, arg):
-    if self.argval (arg) == 0:
+  def div (self, oper):
+    if self.decoper (oper) == 0:
       raise Exception ("Division by zero")
-    self.reg[0] /= self.argval (arg)
+    self.reg[0] /= self.decoper (oper)
     self.pc += 1
-    self.addtime (arg, True)
+    self.addtime (oper, True)
 
-  def goto  (self, arg):
-    self.pc = self.argval (arg)
+  def goto  (self, oper):
+    self.pc = int (oper)
     self.addtime ("1")
 
-  def jzero (self, arg):
+  def jzero (self, oper):
     if self.reg[0] == 0:
-      self.pc = self.argval (arg)
+      self.pc = int (oper)
     else:
       self.pc += 1
-    self.addtime ("#0")
+    self.addtime ("0")
 
   def end (self):
     self.pc = 0
     self.addtime ("1")
 
-  def cost (self, arg):
-    if arg == 0:
+  def cost (self, oper):
+    if oper == 0:
       return 1
     else:
-      return int (log (arg, 2)) + 1
+      return int (log (oper, 2)) + 1
 
-  def addspace (self, arg):
+  def addspace (self, oper):
     i = 0
-    if arg[0] == "#" or arg[0] == "*":
-      i = int (arg[1:])
+    if oper[0] == "#" or oper[0] == "*":
+      i = int (oper[1:])
     if self.cost (self.reg[i]) > self.space[i]:
       self.space[i] = self.cost (self.reg[i])
 
-  def addtime (self, arg, addacc = False):
-    if "#" == arg[0]:
-      self.time += self.cost (int (arg[1:]))
-      self.time += self.cost (self.reg[int (arg[1:])])
-    elif "*" == arg[0]:
-      self.time += self.cost (int (arg[1:]))
-      self.time += self.cost (self.reg[int (arg[1:])])
-      self.time += self.cost (self.reg[self.reg [(int (arg[1:]))]])
+  def addtime (self, oper, addacc = False):
+    if "#" == oper[0]:
+      self.time += self.cost (int (oper[1:]))
+      self.time += self.cost (self.reg[int (oper[1:])])
+    elif "*" == oper[0]:
+      self.time += self.cost (int (oper[1:]))
+      self.time += self.cost (self.reg[int (oper[1:])])
+      self.time += self.cost (self.reg[self.reg [(int (oper[1:]))]])
     else:
-      self.time += self.cost (int (arg))
+      self.time += self.cost (int (oper))
     if addacc:
       self.time += self.cost (self.reg[0])
 
-  def argval (self, arg):
+  def decoper (self, oper):
     """
-    decode integer argument
+    decode operand
     """
-    if "*" == arg[0]:
-      return self.reg[self.reg [(int (arg[1:]))]]
-    elif "#" == arg[0]:
-      return self.reg[int (arg[1:])]
+    if "*" == oper[0]:
+      return self.reg[self.reg [(int (oper[1:]))]]
+    elif "#" == oper[0]:
+      return int (oper[1:])
     else:
-      return int (arg)
+      return self.reg[int (oper)]
 
-  def regindex (self, arg):
+  def regindex (self, oper):
     """
     decode reg[index] argument
     """
-    if "*" == arg[0]:
-      return self.reg [(int (arg[1:]))]
-    elif "#" == arg[0]:
-      return int (arg[1:])
+    if "*" == oper[0]:
+      return self.reg [(int (oper[1:]))]
+    else:
+      return int (oper)
 
 if __name__ == "__main__":
   if len (sys.argv) == 2:
@@ -273,17 +273,17 @@ if __name__ == "__main__":
 # first line is register initialization
 0 3 6
 # program text starts here
-1  LOAD 0
-2  STORE #3
-3  LOAD #1
+1  LOAD #0
+2  STORE 3
+3  LOAD 1
 4  JZERO 11
-5  SUB 1
-6  STORE #1
-7  LOAD #2
-8  ADD #3
-9  STORE #3
+5  SUB #1
+6  STORE 1
+7  LOAD 2
+8  ADD 3
+9  STORE 3
 10 GOTO 3
-11 LOAD #3
+11 LOAD 3
 12 END END
 # EOF"""
     outfile.write (p)
